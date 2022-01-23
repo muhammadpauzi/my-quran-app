f29829ebc425a8774efbb54f2cd3ea231f10428d
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-01-23 13:39:49 +0700

    fix: responsive of Container component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children, withPadding = true, props }) {
     return (
-        <div {...props} className={`max-w-4xl mx-auto${withPadding ? ' p-5 py-8' : ''}`} >
+        <div {...props} className={`w-full sm:max-w-4xl mx-auto px-2${withPadding ? ' sm:px-5 py-8' : ' '}`} >
             {children}
         </div >
     );
