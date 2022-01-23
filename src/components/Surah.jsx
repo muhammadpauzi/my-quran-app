26cfd6bbac58cf701900b8482356eae7c3991537
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-01-23 13:45:52 +0700

    fix: responsive of title group element

--- src/components/Surah.jsx
+++ src/components/Surah.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 export default function Surah({ surah }) {
     return (
         <>
-            <div className="flex items-center justify-between mb-5 flex-col sm:flex-row">
-                <Heading className="text-lg sm:text-3xl font-bold mb-5 sm:mb-0">Surah</Heading>
-                <Input withLabel={false} placeholder="Enter a keyword..." nameAndID="keyword" className="w-72" />
+            <div className="flex items-stretch sm:items-center justify-between mb-5 flex-col sm:flex-row">
+                <Heading className="text-lg sm:text-3xl font-bold mb-5 sm:mb-0 text-center">Surah</Heading>
+                <Input withLabel={false} placeholder="Enter a keyword..." nameAndID="keyword" className="w-full sm:w-72" />
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 ">
--- src/components/SurahDetail.jsx
+++ src/components/SurahDetail.jsx
@@ -4,9 +4,9 @@ import Input from './Input';
 export default function SurahDetail() {
     return (
         <>
-            <div className="flex items-center justify-between mb-5 flex-col sm:flex-row">
-                <Heading className="text-lg sm:text-3xl font-bold mb-5 sm:mb-0">Surah Al-Fatihah</Heading>
-                <Input withLabel={false} placeholder="Enter a keyword..." nameAndID="keyword" className="w-72" />
+            <div className="flex items-stretch sm:items-center justify-between mb-5 flex-col sm:flex-row">
+                <Heading className="text-lg sm:text-3xl font-bold mb-5 sm:mb-0 text-center">Surah Al-Fatihah</Heading>
+                <Input withLabel={false} placeholder="Enter a keyword..." nameAndID="keyword" className="w-full sm:w-72" />
             </div>
 
             <div className="space-y-5">
