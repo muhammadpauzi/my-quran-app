db5e9e685673e6586844be49663f7ca6dc366933
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-01-25 20:57:42 +0700

    fix&refactor: set to favorite

--- src/components/SurahCard.jsx
+++ src/components/SurahCard.jsx
@@ -5,39 +5,34 @@ import { getCurrentData, saveData } from '../helpers/localStorage';
 import { useEffect, useState } from "react";
 
 export default function SurahCard({ number, englishName, englishNameTranslation, numberOfAyahs }) {
-
     return (
         <Card>
             <div className="flex items-center justify-between mb-4">
                 <span className="rounded-full h-7 w-7 flex items-center justify-center bg-green-200 text-green-500 text-sm font-bold">{number}</span>
                 <div>
-                    <HeartIcon className={`favorite-buttons block h-6 w-6 ${getCurrentData("_favorites").some(fav => fav.number == number) ? 'text-green-500 hover:text-gray-300' : 'text-gray-300 hover:text-green-500'} cursor-pointer transition hover:scale-125`} onClick={(e) => {
+                    <HeartIcon className={`favorite-buttons block h-6 w-6 ${getCurrentData("_favorites").some(fav => fav.number == number) ? 'text-green-500' : 'text-gray-300'} cursor-pointer transition hover:scale-125`} onClick={(e) => {
                         e.stopPropagation();
                         e.preventDefault();
 
                         let favorites = getCurrentData("_favorites")
-                        if (favorites.some(fav => fav.number == number)) {
-                            favorites = favorites.filter(fav => fav.number != number);
 
-                            if (e.target.classList.contains('favorite-buttons')) {
-                                e.target.classList.replace('text-green-500', 'text-gray-300');
-                                e.target.classList.replace('hover:text-gray-500', 'hover:text-green-500');
-                            } else if (e.target.parentElement.classList.contains('favorite-buttons')) {
-                                e.target.parentElement.classList.replace('text-green-500', 'text-gray-300');
-                                e.target.parentElement.classList.replace('hover:text-gray-500', 'hover:text-green-500');
+                        let element = null;
+                        element = e.target.classList.contains('favorite-buttons') && e.target;
+                        element = e.target.parentElement.classList.contains('favorite-buttons') && e.target.parentElement;
+
+                        const isFavorited = favorites.some(fav => fav.number == number);
+
+                        if (isFavorited) {
+                            favorites = favorites.filter(fav => fav.number != number); // remove favorite with the number of surah in localStorage
+                            if (element) {
+                                element.classList.replace('text-green-500', 'text-gray-300');
                             };
                         } else {
-                            if (e.target.classList.contains('favorite-buttons')) {
-                                e.target.classList.replace('text-gray-300', 'text-green-500');
-                                e.target.classList.replace('hover:text-green-500', 'hover:text-gray-500');
-                            } else if (e.target.parentElement.classList.contains('favorite-buttons')) {
-                                e.target.parentElement.classList.replace('text-gray-300', 'text-green-500');
-                                e.target.parentElement.classList.replace('hover:text-green-500', 'hover:text-gray-500');
+                            if (element) {
+                                element.classList.replace('text-gray-300', 'text-green-500');
                             };
-
                             favorites.push({ number });
                         }
-                        document.body.focus(); // for remove hover state in mobile after click it
                         saveData('_favorites', favorites);
                     }} />
                 </div>
