fac877703235c7c1cef836df670d6c268d651e22
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-01-30 13:25:53 +0700

    fix: support page

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,14 +1,21 @@
 import Heading from "./Heading";
+import saweriaIcon from '../images/saweria-icon.png';
 
 export default function Support() {
     return (
         <div className="py-5">
-            <Heading className="text-xl sm:text-3xl font-bold mb-10">Support Me</Heading>
-            <div className="flex gap-2 items-center">
-                <a href="https://trakteer.id/muhammad-pauzi" className="p-10 bg-white text-green-500 font-bold mb-4 border-2 hover:border-green-500 transition rounded">
+            <Heading className="text-3xl text-center sm:text-left font-bold mb-10">Support Me</Heading>
+            <div className="flex gap-2 items-stretch flex-col w-full sm:w-auto sm:flex-row text-center">
+                <a target="_blank" href="https://trakteer.id/muhammad-pauzi" className="px-8 py-10 bg-white text-green-500 grid place-content-center font-bold mb-4 border-2 hover:border-green-500 transition rounded">
+                    <div className="px-5 py-4 mb-4 bg-red-800 grid place-content-center rounded">
+                        <img src="https://trakteer.id/images/mix/navbar-logo-lite-white-beta.png" alt="" className="block w-40" />
+                    </div>
                     Support with Trakteer
                 </a>
-                <a href="https://trakteer.id/muhammad-pauzi" className="p-10 bg-white text-green-500 font-bold mb-4 border-2 hover:border-green-500 transition rounded">
+                <a target="_blank" href="https://saweria.co/muhammadpauzi" className="px-8 py-10 bg-white text-green-500 grid place-content-center font-bold mb-4 border-2 hover:border-green-500 transition rounded">
+                    <div className="px-5 py-4 mb-4 grid place-content-center rounded">
+                        <img src={saweriaIcon} alt="" className="block w-40" />
+                    </div>
                     Support with Saweria
                 </a>
             </div>
