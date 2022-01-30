b858e42c99049b35941cd8eaf185917c8bb92084
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-01-30 12:58:41 +0700

    content: update footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
+import { HeartIcon } from "@heroicons/react/solid";
 import Container from "./Container";
 
 export default function Footer() {
     return (
-        <div className="text-center py-6 px-5 bg-white shadow-lg shadow-slate-100">
+        <div className="text-center py-6 px-5 bg-white drop-shadow-lg  shadow-slate-100">
             <Container withPadding={false}>
-                <p className="text-gray-800">Created by <a href="" className="text-green-500 font-bold">Muhammad Pauzi.</a></p>
+                <p className="text-gray-800">Created by <a href="https://github.com/muhammadpauzi" className="text-green-500 font-bold">Muhammad Pauzi</a> with <HeartIcon className="inline-block h-5 w-5 text-red-500" /> and save in <a href="https://github.com/muhammadpauzi" className="text-green-500 font-bold">Github.</a></p>
             </Container>
         </div>
     )
